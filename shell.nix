7e5bfcf93d93650e4537d3cc44970508efac0fa2
iktefish <87042742+iktefish@users.noreply.github.com>
2022-02-11 12:51:33 +0600

    TypeScript setup

--- shell.nix
+++ shell.nix
@@ -5,6 +5,9 @@ let
       https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
   pkgsUnstable = import unstableTarball { };
   nodeNix = import ~/Arcane/_n/node/default.nix { };
+  ts-node = pkgs.writeShellScriptBin ''ts-node'' ''$PROJECT_ROOT/node_modules/.bin/ts-node "$@"'';
+  tsc = pkgs.writeShellScriptBin ''tsc'' ''$PROJECT_ROOT/node_modules/.bin/tsc "$@"'';
+  nodemon = pkgs.writeShellScriptBin ''nodemon'' ''$PROJECT_ROOT/node_modules/.bin/nodemon "$@"'';
   scripts = pkgs.writeShellScriptBin ''scripts'' ''jq ".scripts" package.json'';
   run = pkgs.writeShellScriptBin ''run'' ''exec npm run "$@"'';
 
@@ -20,6 +23,9 @@ stdenv.mkDerivation {
 
     jq
 
+    ts-node
+    tsc
+    nodemon
     scripts
     run
   ];
